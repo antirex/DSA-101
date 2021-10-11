2dc3900dee02fbeeba14c19bcf613e693c934bee
Ansh <55479324+antirex@users.noreply.github.com>
2021-10-11 14:48:34 +0530

    update TC

--- two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.java
+++ two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.java
@@ -1,9 +1,19 @@
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
-        for(int i =0;i<numbers.length-1;i++){
-            for(int j = i+1;j<numbers.length;j++){
-                if(numbers[i]+numbers[j]==target) return new int[]{i+1,j+1};
-            }
+//         for(int i =0;i<numbers.length-1;i++){ //o(n^2)
+//             for(int j = i+1;j<numbers.length;j++){
+//                 if(numbers[i]+numbers[j]==target) return new int[]{i+1,j+1};
+//             }
+//         }
+//         return new int[]{-1,-1};
+
+        int r = numbers.length-1;
+        int l =0;
+        while(l<r){ //O(n) Two pointer approach
+            int temp = numbers[l] + numbers [r];
+            if(temp == target) return new int[]{l+1,r+1};
+            else if(temp>target) r--;
+            else l++;
         }
         return new int[]{-1,-1};
     }
