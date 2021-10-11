2936b94fcc79c7c80463dccc0026caaaf5a4aeac
Ansh <55479324+antirex@users.noreply.github.com>
2021-10-11 10:51:13 +0530

    feat: update code

--- squares-of-a-sorted-array/squares-of-a-sorted-array.java
+++ squares-of-a-sorted-array/squares-of-a-sorted-array.java
@@ -1,10 +1,23 @@
+//Runtime: 6 ms, faster than 12.70% of Java online submissions for Squares of a Sorted Array.
 class Solution {
     public int[] sortedSquares(int[] nums) {
-        int res[] = new int[nums.length];
         for(int i =0;i<nums.length;i++){
-            res[i] = (int)Math.pow(nums[i],2);
+            nums[i] = (int)Math.pow(nums[i],2);
         }
-        Arrays.sort(res);
-        return res;
+        Arrays.sort(nums);
+        return nums;
     }
 }
+
+
+
+// class Solution {
+//     public int[] sortedSquares(int[] nums) {
+//         int res[] = new int[nums.length];
+//         for(int i =0;i<nums.length;i++){
+//             res[i] = (int)Math.pow(nums[i],2);
+//         }
+//         Arrays.sort(res);
+//         return res;
+//     }
+// }
