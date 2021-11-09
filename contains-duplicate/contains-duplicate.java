03d986ba5a5b9267482824efcbf9ab28410f276e
Ansh <55479324+antirex@users.noreply.github.com>
2021-11-09 10:17:56 +0530

    update

--- contains-duplicate/contains-duplicate.java
+++ contains-duplicate/contains-duplicate.java
@@ -1,17 +1,24 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        Arrays.sort(nums);
-        for(int i = 0;i<nums.length;i++){
-            int left = i+1;
-            int right = nums.length;
-            int temp = nums[i];
-            while(left<right){
-                int mid = left + (right - left)/2;
-                if(nums[mid] == temp ) return true;
-                else if(nums[mid]>temp) right = mid;
-                else left = mid + 1;
-            }
+        
+        HashSet<Integer> set = new HashSet<>();
+        for(int i = 0; i<nums.length;i++){
+            if(set.add(nums[i])) continue;
+            else return true;
         }
         return false;
+        // Arrays.sort(nums);
+        // for(int i = 0;i<nums.length;i++){
+        //     int left = i+1;
+        //     int right = nums.length;
+        //     int temp = nums[i];
+        //     while(left<right){
+        //         int mid = left + (right - left)/2;
+        //         if(nums[mid] == temp ) return true;
+        //         else if(nums[mid]>temp) right = mid;
+        //         else left = mid + 1;
+        //     }
+        // }
+        // return false;
     }
 }
