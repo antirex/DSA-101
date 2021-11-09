e9c35786e63a0a81fa473ad740149d1b910b6483
Ansh <55479324+antirex@users.noreply.github.com>
2021-11-09 22:22:49 +0530

    Iterative + recursive --> reverse LL

--- reverse-linked-list/reverse-linked-list.java
+++ reverse-linked-list/reverse-linked-list.java
@@ -10,24 +10,44 @@
  */
 class Solution {
     public ListNode reverseList(ListNode head) {
-        ListNode ptr = new ListNode();
-        recur(new ListNode(0,head), ptr);
-        return ptr.next;
-    }
-    private ListNode recur(ListNode head, ListNode newNode){
-        if(head.next == null) return newNode;
-        ListNode curr = head;
-        ListNode prev = null;
-        while(curr!=null){
-            if(curr.next == null){
-                newNode.next = new ListNode(curr.val);
-                prev.next = null;
-                break;
-            }else{
-                prev = curr;  
-                curr = curr.next;
-            }
+        //Iterative Approach
+        ListNode reverse = new ListNode();
+        Stack<Integer> stack = new Stack<>();
+        ListNode ptr = head;
+        ListNode newPtr = reverse;
+        while(ptr != null){
+            stack.push(ptr.val);
+            ptr = ptr.next;
         }
-        return recur(head, newNode.next);
+        while(!stack.isEmpty()){
+            newPtr.next = new ListNode(stack.pop());
+            newPtr = newPtr.next;
+        }
+        return reverse.next;
     }
 }
+
+
+
+// Recursive approach
+ // public ListNode reverseList(ListNode head) {
+ //        ListNode ptr = new ListNode();
+ //        recur(new ListNode(0,head), ptr);
+ //        return ptr.next;
+ //    }
+ //    private ListNode recur(ListNode head, ListNode newNode){
+ //        if(head.next == null) return newNode;
+ //        ListNode curr = head.next;
+ //        ListNode prev = head;
+ //        while(curr!=null){
+ //            if(curr.next == null){
+ //                newNode.next = new ListNode(curr.val);
+ //                prev.next = null;
+ //                break;
+ //            }else{
+ //                prev = curr;  
+ //                curr = curr.next;
+ //            }
+ //        }
+ //        return recur(head, newNode.next);
+ //    }
