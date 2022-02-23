b7f518114efa161a0ba4b019a2ecefe263daa9d7
Ansh <55479324+antirex@users.noreply.github.com>
2022-02-23 19:15:36 +0530

    Added solution - LeetHub

---
+++ Rotation - GFG/rotation.java
@@ -0,0 +1,47 @@
+// { Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    // Driver code
+    public static void main(String[] args) throws Exception {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+        while (t-- > 0) {
+            int n = Integer.parseInt(br.readLine().trim());
+            String[] str = br.readLine().trim().split(" ");
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = Integer.parseInt(str[i]);
+            }
+
+            int ans = new Solution().findKRotation(a, n);
+            System.out.println(ans);
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    int findKRotation(int arr[], int n) {
+        // code here
+        int start = 0;
+        int end = n - 1;
+        while(start<=end){
+            int mid = start + (end - start)/2;
+            int next = (mid+1)%n;
+            int prev = (mid+n-1)%n;
+            if(arr[mid]<=arr[prev] && arr[mid]<=arr[next]) return mid;
+            if(arr[start]<=arr[mid] && arr[mid]<=arr[end]) return start;
+            if(arr[start]<=arr[mid]) start = mid +1;
+            else if(arr[mid]<=arr[end]) end = mid - 1;
+        }
+        return 0;
+    }
+}
