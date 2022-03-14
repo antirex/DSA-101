5b948e8f8294eacc92746a91a5ae95fd1f9893ca
Ansh <55479324+antirex@users.noreply.github.com>
2022-03-14 22:23:07 +0530

    Time: 14 ms (24.87%), Space: 51.6 MB (6.26%) - LeetHub

---
+++ 229-majority-element-ii/229-majority-element-ii.java
@@ -0,0 +1,19 @@
+class Solution {
+    public List<Integer> majorityElement(int[] nums) {
+        int max = nums.length/3;
+        List<Integer> list = new ArrayList<>();
+//         int[] freq = new int[nums.length+1];
+        
+//         for(int i=0; i<nums.length;i++) freq[nums[i]]++;
+//         for(int i=0; i<freq.length;i++) if(freq[i]>max) list.add(i);
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int i =0;i<nums.length;i++){
+            if(!map.containsKey(nums[i])) map.put(nums[i], 1);
+            else map.put(nums[i], map.getOrDefault(nums[i],1)+1);
+        }
+        for(int i =0;i<nums.length;i++){
+            if(map.get(nums[i])>max && !list.contains(nums[i])) list.add(nums[i]);
+        }
+        return list;
+    }
+}
