2eea28d4b01bddf162e199eaa34e002fcb1fb4c3
Ansh <55479324+antirex@users.noreply.github.com>
2022-03-25 10:15:46 +0530

    Added solution - LeetHub

--- Arithmetic Number - GFG/arithmetic-number.java
+++ Arithmetic Number - GFG/arithmetic-number.java
@@ -27,13 +27,8 @@ class GFG{
 class Solution{
     static int inSequence(int A, int B, int C){
         // code here
-        // tn = A+(n-1)C
         if(A==B) return 1;
-        if(C==0){
-            if(A==B) return 1;
-            else return 0;
-        };
-       
+        if(C==0) return 0;
         if((B-A)%C==0 && (B-A)/C>0) return 1;
         return 0;
     }
