7311a1fcd6c249588a63aedab6708eb2387e167f
Ansh <55479324+antirex@users.noreply.github.com>
2022-04-10 14:49:09 +0530

    Added solution - LeetHub

---
+++ Permutations of a given string - GFG/permutations-of-a-given-string.java
@@ -0,0 +1,56 @@
+// { Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+	public static void main(String[] args) throws IOException
+	{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+        while(t-->0)
+        {
+            String S = br.readLine().trim();
+            Solution obj = new Solution();
+            List<String> ans = obj.find_permutation(S);
+            for( int i = 0; i < ans.size(); i++)
+            {
+                System.out.print(ans.get(i)+" ");
+            }
+            System.out.println();
+                        
+        }
+	}
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    public List<String> find_permutation(String S) {
+        TreeSet<Character> chars = new TreeSet<>();
+        for(int i=0;i<S.length();++i) chars.add(S.charAt(i));
+        List<String> res = new ArrayList<>();
+        recur(chars, res, "", S.length());
+        return res;
+    }
+    
+    private void recur(
+        TreeSet<Character> chars, 
+        List<String> result, 
+        String str, 
+        int k
+    ) {
+        if (str.length() == k) {
+            result.add(str);
+            return;
+        }
+        
+        TreeSet<Character> chars2 = new TreeSet<>(chars);
+        for(Character ch: chars) {
+            chars2.remove(ch);
+            recur(chars2, result, str+ch, k);
+            chars2.add(ch);
+        }
+    }
+}
