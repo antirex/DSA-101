bcd6066e2c89aab21ecfdfd94e6de0fd4cb0cd9d
Ansh <55479324+antirex@users.noreply.github.com>
2022-04-20 11:40:49 +0530

    Added solution - LeetHub

---
+++ Move all negative elements to end - GFG/move-all-negative-elements-to-end.java
@@ -0,0 +1,54 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main(String[] args) throws IOException
+	{
+	        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t =
+            Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while(t-->0)
+        {
+            int n = Integer.parseInt(br.readLine().trim());
+            int a[] = new int[(int)(n)];
+            String inputLine[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                a[i] = Integer.parseInt(inputLine[i]);
+            }
+            
+            Solution obj = new Solution();
+            obj.segregateElements(a, n);
+            
+            for(int i=0;i<n;i++)
+            System.out.print(a[i]+" ");
+            
+            System.out.println();
+        }
+	}
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    
+    public void segregateElements(int arr[], int n)
+    {
+        // Your code goes here
+        int start=0, end = 0;
+        ArrayList<Integer> list = new ArrayList<>();
+        while(end<n){
+            if(arr[end]>-1) arr[start++] = arr[end];
+            else list.add(arr[end]);
+            end++;
+        }
+        end=0;
+        while(start<n) arr[start++] = list.get(end++);
+    }
+}
